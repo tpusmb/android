9bcf3f7bc7e65c1822d55db8761d96f9974e741c
ClemiNeko <c.lebras776@laposte.net>
2018-09-13 15:12:18 +0100

    Add a button to access camera and take a picture. Add an imageView to display that picture.

--- app/src/main/java/com/example/clemineko/humananime/MainActivity.java
+++ app/src/main/java/com/example/clemineko/humananime/MainActivity.java
@@ -1,13 +1,42 @@
 package com.example.clemineko.humananime;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnCamera;
+    ImageView imgView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnCamera = findViewById(R.id.btnCamera);
+        imgView = findViewById(R.id.imgView);
+
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent,0);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(data != null) {
+            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+            imgView.setImageBitmap(bitmap);
+        }
     }
 }
