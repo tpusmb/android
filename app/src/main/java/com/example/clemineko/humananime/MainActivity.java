b13d82e5a903f7c5c52b56f597e872bf09e476fc
ClemiNeko <c.lebras776@laposte.net>
2018-09-15 18:35:21 +0100

    Added a button to import an image from the image gallery.

--- app/src/main/java/com/example/clemineko/humananime/MainActivity.java
+++ app/src/main/java/com/example/clemineko/humananime/MainActivity.java
@@ -2,18 +2,27 @@ package com.example.clemineko.humananime;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final int CAMERA_REQUEST = 10;
+    public static final int GALLERY_REQUEST = 20;
 
     Button btnCamera;
+    Button btnGallery;
     ImageView imgView;
 
     @Override
@@ -22,26 +31,50 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnCamera = findViewById(R.id.btnCamera);
+        btnGallery = findViewById(R.id.btnGallery);
         imgView = findViewById(R.id.imgView);
-
-        btnCamera.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(intent, CAMERA_REQUEST);
-            }
-        });
     }
+
+
+    public void onCameraButtonClicked(View v){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, CAMERA_REQUEST);
+    }
+
+    public void onGalleryButtonClicked(View v){
+        Intent intent = new Intent(Intent.ACTION_PICK);
+
+        File pictureDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        Uri data = Uri.parse(pictureDirectory.getPath());
+
+        intent.setDataAndType(data, "image/*");
+        startActivityForResult(intent, GALLERY_REQUEST);
+    }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+
         if(resultCode == RESULT_OK) {
+            Bitmap bitmap;
+
             if(requestCode == CAMERA_REQUEST){
-                Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+                bitmap = (Bitmap) data.getExtras().get("data");
                 imgView.setImageBitmap(bitmap);
             }
+            else if(requestCode == GALLERY_REQUEST){
+                Uri imageUri = data.getData();
 
+                try{
+                    InputStream IS = getContentResolver().openInputStream(imageUri);
+                    bitmap = BitmapFactory.decodeStream(IS);
+                    imgView.setImageBitmap(bitmap);
+                } catch (Exception e){
+                    e.printStackTrace();
+                    Toast.makeText(this, "Unable to open image", Toast.LENGTH_LONG).show();
+                }
+            }
         }
     }
 }
