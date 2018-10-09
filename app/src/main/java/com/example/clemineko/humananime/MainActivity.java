2dece923ef8d02d16140127a9e24a293ee7775d2
ClemiNeko <c.lebras776@laposte.net>
2018-10-09 14:07:58 +0100

    Started adding image.

--- app/src/main/java/com/example/clemineko/humananime/MainActivity.java
+++ app/src/main/java/com/example/clemineko/humananime/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
