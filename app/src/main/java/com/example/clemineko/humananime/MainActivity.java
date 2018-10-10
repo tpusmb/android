fc6367b15b466808cc56e4956f936e8ad6d970c7
ClemiNeko <c.lebras776@laposte.net>
2018-10-10 09:52:41 +0100

    Removed unused imports.

--- app/src/main/java/com/example/clemineko/humananime/MainActivity.java
+++ app/src/main/java/com/example/clemineko/humananime/MainActivity.java
@@ -4,8 +4,6 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
